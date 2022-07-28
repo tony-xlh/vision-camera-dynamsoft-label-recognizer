462df54f911d825b8be9175f274f615e0a3fcb40
xulihang <xulihanghai@163.com>
2022-07-28 14:42:14 +0800

    use license in params

--- android/src/main/java/com/visioncameradynamsoftlabelrecognizer/VisionCameraDLRPlugin.java
+++ android/src/main/java/com/visioncameradynamsoftlabelrecognizer/VisionCameraDLRPlugin.java
@@ -3,15 +3,12 @@ package com.visioncameradynamsoftlabelrecognizer;
 import android.annotation.SuppressLint;
 import android.graphics.Bitmap;
 import android.util.Log;
-
 import androidx.camera.core.ImageProxy;
 
-import com.dynamsoft.core.ImageData;
+import com.facebook.react.bridge.ReadableNativeMap;
 import com.facebook.react.bridge.WritableNativeArray;
 import com.mrousavy.camera.frameprocessor.FrameProcessorPlugin;
 import com.dynamsoft.dlr.*;
-
-import java.nio.ByteBuffer;
 
 public class VisionCameraDLRPlugin extends FrameProcessorPlugin {
 
@@ -19,9 +16,14 @@ public class VisionCameraDLRPlugin extends FrameProcessorPlugin {
     @Override
     public Object callback(ImageProxy image, Object[] params) {
         // code goes here
+        ReadableNativeMap config = getConfig(params);
+        String license = "DLS2eyJoYW5kc2hha2VDb2RlIjoiMjAwMDAxLTE2NDk4Mjk3OTI2MzUiLCJvcmdhbml6YXRpb25JRCI6IjIwMDAwMSIsInNlc3Npb25QYXNzd29yZCI6IndTcGR6Vm05WDJrcEQ5YUoifQ==";
+        if (config != null && config.hasKey("license")) {
+          license = config.getString("license");
+        }
         if (recognizer == null) {
             Log.d("DLR","init");
-            initDLR();
+            initDLR(license);
         }
         WritableNativeArray array = new WritableNativeArray();
         @SuppressLint("UnsafeOptInUsageError")
@@ -42,8 +44,8 @@ public class VisionCameraDLRPlugin extends FrameProcessorPlugin {
         return array;
     }
 
-    private void initDLR() {
-        LabelRecognizer.initLicense("DLS2eyJvcmdhbml6YXRpb25JRCI6IjIwMDAwMSIsInByb2R1Y3RzIjoyfQ==", new DLRLicenseVerificationListener() {
+    private void initDLR(String license) {
+        LabelRecognizer.initLicense(license, new DLRLicenseVerificationListener() {
             @Override
             public void DLRLicenseVerificationCallback(boolean isSuccess, Exception error) {
                 if(!isSuccess){
@@ -58,6 +60,15 @@ public class VisionCameraDLRPlugin extends FrameProcessorPlugin {
         }
     }
 
+    private ReadableNativeMap getConfig(Object[] params){
+        if (params.length>0) {
+            if (params[0] instanceof ReadableNativeMap) {
+                ReadableNativeMap config = (ReadableNativeMap) params[0];
+                return config;
+            }
+        }
+        return null;
+    }
 
     VisionCameraDLRPlugin() {
         super("recognize");
