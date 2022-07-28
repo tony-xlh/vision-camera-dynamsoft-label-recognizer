6aa0a4d4cd8d34ebe0d2a3897c2ff9480f7cd949
xulihang <xulihanghai@163.com>
2022-07-28 14:38:01 +0800

    add barcode mask to the example

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -3,12 +3,16 @@ import * as React from 'react';
 import { StyleSheet, SafeAreaView } from 'react-native';
 import { recognize, DLRConfig } from 'vision-camera-dynamsoft-label-recognizer';
 import { Camera, useCameraDevices, useFrameProcessor } from 'react-native-vision-camera';
+import BarcodeMask from 'react-native-barcode-mask';
 
+import { Dimensions } from 'react-native';
 
 export default function App() {
   const [hasPermission, setHasPermission] = React.useState(false);
   const devices = useCameraDevices();
   const device = devices.back;
+  const [maskHeight,setMaskHeight] = React.useState(100);
+  const [maskWidth,setMaskWidth] = React.useState(300);
   
   const frameProcessor = useFrameProcessor((frame) => {
     'worklet'
@@ -22,6 +26,10 @@ export default function App() {
       const status = await Camera.requestCameraPermission();
       setHasPermission(status === 'authorized');
     })();
+    const windowWidth = Dimensions.get('window').width;
+    const windowHeight = Dimensions.get('window').height;
+    setMaskWidth(windowWidth*0.8)
+    setMaskHeight(windowHeight*0.15)
   }, []);
 
  return (
@@ -35,7 +43,9 @@ export default function App() {
         isActive={true}
         frameProcessor={frameProcessor}
         frameProcessorFps={1}
-        />
+        >
+        </Camera>
+        <BarcodeMask width={maskWidth} height={maskHeight} />
       </>)}
     </SafeAreaView>
   );
