73cf8c8435d5d54290bcb567d32633e6df0614fc
xulihang <xulihanghai@163.com>
2022-08-01 00:18:16 -0700

    iOS: add DLR instance

--- ios/VisionCameraDLRPlugin.swift
+++ ios/VisionCameraDLRPlugin.swift
@@ -7,12 +7,52 @@
 //
 
 import Foundation
+import DynamsoftLabelRecognizer
 
 @objc(VisionCameraDLRPlugin)
 public class VisionCameraDLRPlugin: NSObject, FrameProcessorPluginBase {
+    private static var recognizer:DynamsoftLabelRecognizer!
+    private static let context = CIContext(options: nil)
     @objc
     public static func callback(_ frame: Frame!, withArgs args: [Any]!) -> Any! {
-         // code goes here
-        return ["asd"]
+        let config = getConfig(withArgs: args)
+        if recognizer == nil {
+            initDLR()
+        }
+        guard let imageBuffer = CMSampleBufferGetImageBuffer(frame.buffer) else {
+            print("Failed to get CVPixelBuffer!")
+            return nil
+        }
+        let ciImage = CIImage(cvPixelBuffer: imageBuffer)
+
+        guard let cgImage = context.createCGImage(ciImage, from: ciImage.extent) else {
+          print("Failed to create CGImage!")
+          return nil
+        }
+        var returned_results: [Any] = []
+        let image = UIImage(cgImage: cgImage)
+        var error : NSError? = NSError()
+        let results = recognizer.recognizeByImage(image: image, templateName: "", error: &error)
+        for result in results {
+            for line in result.lineResults! {
+                returned_results.append(line.text!)
+            }
+        }
+
+        return returned_results
     }
+    
+    static func initDLR() {
+        DynamsoftLabelRecognizer.initLicense("DLS2eyJvcmdhbml6YXRpb25JRCI6IjIwMDAwMSJ9", verificationDelegate: self)
+        recognizer = DynamsoftLabelRecognizer.init()
+    }
+
+    static func getConfig(withArgs args: [Any]!) -> [String:String]! {
+        if args.count>0 {
+            let config = args[0] as? [String: String]
+            return config
+        }
+        return nil
+    }
+
 }
