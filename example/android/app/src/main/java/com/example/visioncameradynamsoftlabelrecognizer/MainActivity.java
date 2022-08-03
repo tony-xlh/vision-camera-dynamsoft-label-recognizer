18cdbfa55f8fb0f9b7dc99908f6bf74007d215a5
xulihang <xulihanghai@163.com>
2022-08-03 11:21:50 +0800

    example: add navigation

--- example/android/app/src/main/java/com/example/visioncameradynamsoftlabelrecognizer/MainActivity.java
+++ example/android/app/src/main/java/com/example/visioncameradynamsoftlabelrecognizer/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.visioncameradynamsoftlabelrecognizer;
 
+import android.os.Bundle;
+
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
@@ -35,6 +37,11 @@ public class MainActivity extends ReactActivity {
       // If you opted-in for the New Architecture, we enable the Fabric Renderer.
       reactRootView.setIsFabric(BuildConfig.IS_NEW_ARCHITECTURE_ENABLED);
       return reactRootView;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+      super.onCreate(null);
     }
   }
 }
