9937f3908dbacb2ddc0f116f37292f842ae33d3c
xulihang <xulihanghai@163.com>
2022-08-04 14:58:12 +0800

    add use case dropdown

--- example/src/screens/Home.tsx
+++ example/src/screens/Home.tsx
@@ -1,8 +1,11 @@
 import React from "react";
 import { SafeAreaView, StyleSheet, Text, TouchableOpacity } from "react-native";
+import SelectDropdown from 'react-native-select-dropdown'
+
+const usecases = ["MRZ", "General"]
 
 export default function HomeScreen({route, navigation}) {
-
+  const [selectedUseCase,setSelectedUseCase] = React.useState(0);
   const onPressed = (target:string) => {
     navigation.navigate(
       {
@@ -13,7 +16,27 @@ export default function HomeScreen({route, navigation}) {
 
   return (
     <SafeAreaView style={styles.container}>
-      <Text style={styles.info}>Ready to scan MRZ Passport</Text>
+      <Text style={{margin:8}}>Use Case:</Text>
+      <SelectDropdown
+        data={usecases}
+        defaultValueByIndex={0}
+        buttonStyle={{backgroundColor:"transparent"}}
+        buttonTextStyle={{textAlign:"left",fontSize:15}}
+        defaultButtonText={'Select use case'}
+        onSelect={(selectedItem, index) => {
+          setSelectedUseCase(index);
+        }}
+        buttonTextAfterSelection={(selectedItem, index) => {
+          // text represented after item is selected
+          // if data array is an array of objects then return selectedItem.property to render after item is selected
+          return selectedItem
+        }}
+        rowTextForSelection={(item, index) => {
+          // text represented for each item in dropdown
+          // if data array is an array of objects then return item.property to represent item in dropdown
+          return item
+        }}
+      />
       <TouchableOpacity
         style={styles.button}
         onPress={() => onPressed("Scanner")}
