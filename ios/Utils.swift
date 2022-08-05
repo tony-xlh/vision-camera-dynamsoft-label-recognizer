9934aeb690ef65f63222676d0d0d0be452155ac1
xulihang <xulihanghai@163.com>
2022-08-05 02:39:26 -0700

    add Utils.swift

---
+++ ios/Utils.swift
@@ -0,0 +1,16 @@
+//
+//  Utils.swift
+//  VisionCameraDynamsoftLabelRecognizer
+//
+//  Created by xulihang on 2022/8/5.
+//  Copyright © 2022 Facebook. All rights reserved.
+//
+
+import Foundation
+
+
+class Utils {
+    static func wrapDLRResult () {
+        
+    }
+}
