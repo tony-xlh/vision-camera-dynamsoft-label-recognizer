db38183e1e9c366a8416066a64070b3dc2b9c3ad
xulihang <xulihanghai@163.com>
2022-08-08 15:22:25 +0800

    show scan image

--- example/src/screens/Scanner.tsx
+++ example/src/screens/Scanner.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 
-import { StyleSheet, SafeAreaView, Alert, Modal, Pressable, Text, View, Platform } from 'react-native';
+import { StyleSheet, SafeAreaView, Alert, Modal, Pressable, Text, View, Platform, Image } from 'react-native';
 import { recognize, ScanConfig, ScanRegion, DLRCharacherResult, DLRLineResult, DLRResult } from 'vision-camera-dynamsoft-label-recognizer';
 import { Camera, useCameraDevices, useFrameProcessor } from 'react-native-vision-camera';
 import BarcodeMask from 'react-native-barcode-mask';
@@ -20,6 +20,7 @@ const RecognizedCharacter =(props:{"char":DLRCharacherResult}) =>  {
 
 export default function ScannerScreen({route}) {
   const useCase = route.params.useCase;
+  const [imageData,setImageData] = React.useState(undefined as undefined|string);
   const [isActive,setIsActive] = React.useState(true);
   const [modalVisible, setModalVisible] = React.useState(false);
   const modalVisibleShared = REA.useSharedValue(false);
@@ -56,12 +57,14 @@ export default function ScannerScreen({route}) {
       //config.license = "DLS2eyJoYW5kc2hha2VDb2RlIjoiMjAwMDAxLTE2NDk4Mjk3OTI2MzUiLCJvcmdhbml6YXRpb25JRCI6IjIwMDAwMSIsInNlc3Npb25QYXNzd29yZCI6IndTcGR6Vm05WDJrcEQ5YUoifQ==";
       config.license = "DLS2eyJoYW5kc2hha2VDb2RlIjoiMTAxMDc0MDY2LVRYbE5iMkpwYkdWUWNtOXFYMlJzY2ciLCJvcmdhbml6YXRpb25JRCI6IjEwMTA3NDA2NiJ9";
       config.scanRegion = scanRegion;
+      config.includeImageBase64 = true;
       if (useCase === 0) { //mrz use case
         config.template = "{\"CharacterModelArray\":[{\"DirectoryPath\":\"\",\"FilterFilePath\":\"\",\"Name\":\"NumberUppercase\"}],\"LabelRecognizerParameterArray\":[{\"BinarizationModes\":[{\"BlockSizeX\":0,\"BlockSizeY\":0,\"EnableFillBinaryVacancy\":1,\"LibraryFileName\":\"\",\"LibraryParameters\":\"\",\"Mode\":\"BM_LOCAL_BLOCK\",\"ThreshValueCoefficient\":15}],\"CharacterModelName\":\"NumberUppercase\",\"LetterHeightRange\":[5,1000,1],\"LineStringLengthRange\":[44,44],\"MaxLineCharacterSpacing\":130,\"LineStringRegExPattern\":\"(P[OM<][A-Z]{3}([A-Z<]{0,35}[A-Z]{1,3}[(<<)][A-Z]{1,3}[A-Z<]{0,35}<{0,35}){(39)}){(44)}|([A-Z0-9<]{9}[0-9][A-Z]{3}[0-9]{2}[(01-12)][(01-31)][0-9][MF][0-9]{2}[(01-12)][(01-31)][0-9][A-Z0-9<]{14}[0-9<][0-9]){(44)}\",\"MaxThreadCount\":4,\"Name\":\"locr\",\"TextureDetectionModes\":[{\"Mode\":\"TDM_GENERAL_WIDTH_CONCENTRATION\",\"Sensitivity\":8}],\"ReferenceRegionNameArray\":[\"DRRegion\"]}],\"LineSpecificationArray\":[{\"Name\":\"L0\",\"LineNumber\":\"\",\"BinarizationModes\":[{\"BlockSizeX\":30,\"BlockSizeY\":30,\"Mode\":\"BM_LOCAL_BLOCK\"}]}],\"ReferenceRegionArray\":[{\"Localization\":{\"FirstPoint\":[0,0],\"SecondPoint\":[100,0],\"ThirdPoint\":[100,100],\"FourthPoint\":[0,100],\"MeasuredByPercentage\":1,\"SourceType\":\"LST_MANUAL_SPECIFICATION\"},\"Name\":\"DRRegion\",\"TextAreaNameArray\":[\"DTArea\"]}],\"TextAreaArray\":[{\"LineSpecificationNameArray\":[\"L0\"],\"Name\":\"DTArea\",\"FirstPoint\":[0,0],\"SecondPoint\":[100,0],\"ThirdPoint\":[100,100],\"FourthPoint\":[0,100]}]}";
         config.templateName = "locr";
         config.customModelConfig = {customModelFolder:"MRZ",customModelFileNames:["NumberUppercase","NumberUppercase_Assist_1lIJ","NumberUppercase_Assist_8B","NumberUppercase_Assist_8BHR","NumberUppercase_Assist_number","NumberUppercase_Assist_O0DQ","NumberUppercase_Assist_upcase"]};
       }
       let scanResult = recognize(frame,config);
+
       let results:DLRResult[] = scanResult.results;
       let lineResults:DLRLineResult[] = [];
       for (let index = 0; index < results.length; index++) {
@@ -77,6 +80,10 @@ export default function ScannerScreen({route}) {
       console.log(results);
       if (modalVisibleShared.value === false) { //check is modal visible again since the recognizing process takes time
         if (lineResults.length === 2 || (useCase != 0 && lineResults.length>0)) {
+          if (scanResult.imageBase64) {
+            console.log("has image: ");
+            REA.runOnJS(setImageData)("data:image/jpeg;base64,"+scanResult.imageBase64);
+          }
           REA.runOnJS(setRecognitionResults)(lineResults);
           modalVisibleShared.value = true;
           REA.runOnJS(setModalVisible)(true);
@@ -128,6 +135,19 @@ export default function ScannerScreen({route}) {
     return text.trim();
   }
 
+  const renderImage = () =>{
+    if (imageData) {
+      return (
+        <Image
+          style={styles.logo}
+          source={{
+            uri: imageData,
+          }}
+        />
+      )
+    }
+    return undefined
+  }
   return (
     <SafeAreaView style={styles.container}>
       {device != null &&
@@ -156,6 +176,7 @@ export default function ScannerScreen({route}) {
       >
         <View style={styles.centeredView}>
           <View style={styles.modalView}>
+            {renderImage()}
             {recognitionResults.map((result, idx) => (
               <Text key={"line-"+idx}>
                 {result.characterResults.map((char, idx) => (
@@ -259,5 +280,10 @@ const styles = StyleSheet.create({
   },
   lowConfidenceText:{
     color:"red",
-  }
+  },
+  logo: {
+    width: 150,
+    height: 60,
+    resizeMode: "stretch"
+  },
 });
