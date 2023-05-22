894354e6337a7f563e909de102d4778891e9cdfb
xulihang <xulihanghai@163.com>
2023-05-22 00:27:31 -0700

    update iOS's .m file

--- ios/VisionCameraDynamsoftLabelRecognizer.m
+++ ios/VisionCameraDynamsoftLabelRecognizer.m
@@ -10,11 +10,23 @@
 
 @interface RCT_EXTERN_MODULE(VisionCameraDynamsoftLabelRecognizer, NSObject)
 
-RCT_EXTERN_METHOD(decodeBase64:(NSString *)base64 config:(NSDictionary *)config
+RCT_EXTERN_METHOD(initLicense:(NSString *)license
                  withResolver:(RCTPromiseResolveBlock)resolve
                  withRejecter:(RCTPromiseRejectBlock)reject)
 
-RCT_EXTERN_METHOD(destroy:(RCTPromiseResolveBlock)resolve
+RCT_EXTERN_METHOD(updateTemplate:(NSString *)template
+                 withResolver:(RCTPromiseResolveBlock)resolve
+                 withRejecter:(RCTPromiseRejectBlock)reject)
+
+RCT_EXTERN_METHOD(useCustomModel:customModelConfig:(NSDictionary *)customModelConfig
+                 withResolver:(RCTPromiseResolveBlock)resolve
+                 withRejecter:(RCTPromiseRejectBlock)reject)
+
+RCT_EXTERN_METHOD(decodeBase64:(NSString *)base64
+                 withResolver:(RCTPromiseResolveBlock)resolve
+                 withRejecter:(RCTPromiseRejectBlock)reject)
+
+RCT_EXTERN_METHOD(resetRuntimeSettings:(RCTPromiseResolveBlock)resolve
                  withRejecter:(RCTPromiseRejectBlock)reject)
 
 + (BOOL)requiresMainQueueSetup
